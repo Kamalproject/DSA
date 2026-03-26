12ad2e66bcb460159cc1efe0d63578aadb3c0bcc
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-03-26 23:13:44 +0530

    This java code helps you to find the reverse of the number. That means if user enter a number 45 then the output will be 54.

---
+++ Reverseofnumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Reverseofnumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        int temp=num;
+        int reverse=0;
+        while(num!=0){
+            int rem=num%10;
+            reverse=reverse*10+rem;
+            num=num/10;
+                       
+        }
+        System.out.println("Reverse of "+temp+" is "+reverse);
+    }
+}
