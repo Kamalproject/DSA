fb7489c5dda67ad7a5a9f12d1688123215da63db
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-03-30 23:24:16 +0530

    Trying to solve this probelm using another method. using Xor operatin

--- Missingnumber.java
+++ Missingnumber.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class Missingnumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
-        int n=5;
         int number[]={1,3,4,5};
-        int sum=0;
-        for(int i=0;i<number.length;i++){
-            sum+=number[i];
+        int n=5;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans^=i;
+            
         }
-        int totalsum=n*(n+1)/2;
-        int missingnum=totalsum-sum;
-        System.out.println("Missing number is "+missingnum);
+        
+        for(int i=0;i<number.length;i++){
+            ans =ans^number[i];
+        }
+        System.out.println(ans);
     }
 }
